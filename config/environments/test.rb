113291dd492f9bd68e882b8b312eadfe0cef4913
Ian Seabock <ianseabock@gmail.com>
2021-01-10 12:19:39 -0800

    added users controller

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,8 @@ require "active_support/core_ext/integer/time"
 # and recreated between test runs. Don't rely on the data there!
 
 Rails.application.configure do
+  config.middleware.use Clearance::BackDoor
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.cache_classes = false
@@ -19,11 +21,11 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
+    "Cache-Control" => "public, max-age=#{1.hour.to_i}",
   }
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
